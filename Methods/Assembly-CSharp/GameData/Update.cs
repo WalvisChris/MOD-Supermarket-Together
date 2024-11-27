a4b752067d5f2f5e778a1a68ecac6006bb1c2181
WalvisChris <chris.zuidervaart@gmail.com>
2024-11-27 20:00:28 +0100

    Version 27-11-2024

--- Methods/Assembly-CSharp/GameData/Update.cs
+++ Methods/Assembly-CSharp/GameData/Update.cs
@@ -9,6 +9,7 @@ private void Update()
 	{
 		this.keyPress = true;
 	}
-	this.UpdateFranchisePoints(0, this.gameFranchisePoints);						// Edit: make sure the % is updated every frame
-	if (Input.GetKeyDown(KeyCode.B)) { base.StartCoroutine(this.SpawnTrash()); } 	// Edit: press B to spawn trash
+	if (Input.GetKeyDown(KeyCode.B)) { base.StartCoroutine(this.SpawnTrash()); } 			// Edit: press B to spawn trash
+	if (Input.GetKeyDown(KeyCode.P)) { this.ProgressBarTheme = !this.ProgressBarTheme; } 	// Edit: press P to toggle theme
+	this.UpdateFranchisePoints(0, this.gameFranchisePoints);								// Edit: make sure the % is updated every frame
 }
--- Methods/Assembly-CSharp/GameData/UpdateFranchisePoints.cs
+++ Methods/Assembly-CSharp/GameData/UpdateFranchisePoints.cs
@@ -4,15 +4,15 @@ Show the franchise progress on the TMPro
 
 private void UpdateFranchisePoints(int oldPoints, int newPoints)
 	{
-		string progressText = "Null"
+		string progressText = "Null";
 		if (this.ProgressBarTheme)
 		{
-			int totalBlocks = 20
+			int totalBlocks = 11; // This seems to be the max before it continues on a new line
 			int greenBlocks = Mathf.RoundToInt((this.currentLevelProgress / 100f) * totalBlocks);
-			int whiteBlocks = totalBlocks - greenBlocks
+			int whiteBlocks = totalBlocks - greenBlocks;
 			string greenPart = new string('█', greenBlocks);
-			string whitePart = new string('█', whitePart);
-			progressText.text = $"<color=green>{greenPart}</color><color=white>{whitePart}</color><color=green> " + this.currentLevelProgress.ToString("F1") + "%</color>\n" + gameFranchisePoints.ToString(); 
+			string whitePart = new string('█', whiteBlocks);
+			progressText = $"<color=green>{greenPart}</color><color=white>{whitePart}</color>\n" + gameFranchisePoints.ToString();
 		} else 
 		{
 			progressText = "<color=green>Progress: " + this.currentLevelProgress.ToString("F1") + "%</color>\n" + this.gameFranchisePoints.ToString();   // Edit: custom string display
